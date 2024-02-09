a7ce6a15927b0dfef0384f7ab9422b67338660e8
habib16gb <habib16gb@gmail.com>
2024-02-09 23:30:06 +0100

    finish projet one v1

--- 01 - MERN Auth/backend/server.js
+++ 01 - MERN Auth/backend/server.js
@@ -6,6 +6,7 @@ import { notFound, errorHandler } from "./middlewares/error.middleware.js";
 import DB_Connect from "./config/db.js";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 configDotenv();
 const PORT = process.env.PORT || 3000;
@@ -19,12 +20,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use("/api/users", routerUser);
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => res.send("server is ready"));
+}
+
 app.use(notFound);
 app.use(errorHandler);
-
-// app.get('/',(req, res) => {
-//   res.cookie
-// })
 
 const start = async () => {
   try {
--- 01 - MERN Auth/frontend/src/screens/ProfileScreen.jsx
+++ 01 - MERN Auth/frontend/src/screens/ProfileScreen.jsx
@@ -1,6 +1,6 @@
 import { Form, Button } from "react-bootstrap";
 import FormContainer from "../components/FormContainer";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setCredentials } from "../slices/authSlice.js";
